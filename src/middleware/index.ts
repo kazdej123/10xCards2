3373ac8aecd5476b6b17d962baef1a514036cb3f
Kamil Murawski <k.murawski@comacom.pl>
2025-06-13 17:00:05 +0200

    tmp

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,6 +1,5 @@
 import { defineMiddleware } from "astro:middleware";
-
-import { supabaseClient } from "../db/supabase.client";
+import { supabaseClient } from "../db/supabase.client.ts";
 
 export const onRequest = defineMiddleware((context, next) => {
   context.locals.supabase = supabaseClient;
